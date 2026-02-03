5aef79c27e37549c3f0bbf5dd27c25ee319c2d7d
Samiul2024 <mdsamiullahossen@gmail.com>
2026-02-03 21:03:25 +0600

    Job Details loaded from server...

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 
@@ -38,8 +38,11 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/jobs/:id', async(req,res)=>{
-
+    app.get('/jobs/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
